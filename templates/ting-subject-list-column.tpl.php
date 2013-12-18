cd2866257f1c4cba4a33ee4ba83c2fb374c1cbba
holt83 <simonholt83@gmail.com>
2013-12-18 06:13:38 -0800

    Added templates

---
+++ templates/ting-subject-list-column.tpl.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * HTML for a column in the Ting subject list.
+ *
+ * Available variables:
+ *  - $items:
+ *  	An array of items to be shown in this column. Each items covers a ting 
+ *  	subject and a count of that subject. The array is setup as a render array,
+ *  	so it can be passed directly to the render() function. 
+ *  - $classes: 
+ *    String of classes that can be used to style contextually through
+ *    css. The string can be manipulated through the variable $classes_array in
+ *    preprocess functions.
+ *  - $attributes: 
+ *  	String of attributes to be printed on the container div. The string can 
+ *  	be manipulated through the variable $attributes_array in preprocess 
+ *  	functions.  
+ */
+
+?>
+
