0c133edf2a45e09c11cdc1710b67d040ec70d98b
holt83 <simonholt83@gmail.com>
2013-12-19 06:26:12 -0800

    Added to templates

--- templates/ting-subject-list-column.tpl.php
+++ templates/ting-subject-list-column.tpl.php
@@ -7,7 +7,9 @@
  *  - $items:
  *  	An array of items to be shown in this column. Each items covers a ting 
  *  	subject and a count of that subject. The array is setup as a render array,
- *  	so it can be passed directly to the render() function. 
+ *  	so it can be passed directly to the render() function.
+ *  - $column_title:
+ *  	A string containing the title for this column. 
  *  - $classes: 
  *    String of classes that can be used to style contextually through
  *    css. The string can be manipulated through the variable $classes_array in
@@ -20,6 +22,17 @@
 
 ?>
 
-<div class="<?php print $classes ?>" <?php print $attributes ?>>
-	HEJ
+<div class="<?php print $classes ?>" <?php print $attributes; ?>>
+	<?php if (isset($column_title)): ?>
+		<div class="column-header">
+			<?php print $title_prefix; ?>
+			<h3 <?php print $title_attributes; ?>> <?php print $column_title; ?></h3>
+			<?php print $title_suffix; ?>
+		</div>
+	<?php endif; ?>
+	<?php if (isset($items)): ?>
+		<div class="column-wrapper clearfix" <?php print $content_attributes; ?>>
+			<?php print $items; ?>
+		</div>
+	<?php endif; ?>
 </div>
